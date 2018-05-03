87cb58e339440e479555668b2cad5d8acb20a135
Naveen <naveen241193@gmail.com>
2018-05-03 22:59:16 +0530

    tetrtr

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
 })
 
 export class CartPage{
-    
+    constructor(){
+        this.total();
+    }
+    totalAmt: number = 0;
+    cartItems: any = [
+        {
+            title: 'Chicken Biriyani',
+            quantity: '1',
+            actualPrice: 180.00,
+            totalPrice: 180.00
+        },
+        {
+            title: 'Mutton Biriyani',
+            quantity: '1',
+            actualPrice: 250.00,
+            totalPrice: 250.00
+        }
+    ]
+    add(item){
+        this.cartItems[item].quantity++;
+        this.cartItems[item].totalPrice = this.cartItems[item].actualPrice * this.cartItems[item].quantity;
+        this.total()
+    }
+    remove(item){
+        if(this.cartItems[item].quantity > 0){
+            this.cartItems[item].quantity--;
+            this.cartItems[item].totalPrice = this.cartItems[item].actualPrice * this.cartItems[item].quantity;
+            this.total();
+        }
+    }
+    total(){
+        this.totalAmt = 0;
+        this.cartItems.forEach(element => {
+            this.totalAmt = this.totalAmt + element.totalPrice 
+        });
+    }
 }
